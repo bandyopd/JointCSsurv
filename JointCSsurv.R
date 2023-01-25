50057f050334e59f038596c898d492be1dbb708a
lcyjames <68526415+lcyjames@users.noreply.github.com>
2023-01-25 11:58:24 +0800

    Update JointCSsurv.R

--- JointCSsurv.R
+++ JointCSsurv.R
@@ -1,4 +1,4 @@
-list.of.packages <- c("splines2", "numDeriv","statmod")
+list.of.packages <- c("splines2", "numDeriv", "statmod")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
